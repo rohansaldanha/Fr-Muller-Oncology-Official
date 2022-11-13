b8db59fe263104bc5437983407b12a2d1d39dceb
Rohan Saldanha <rohansaldanha31@gmail.com>
2022-11-13 13:27:31 +0530

    insert schemas written

--- backend/pydantic_schemas/basic_schema.py
+++ backend/pydantic_schemas/basic_schema.py
@@ -2,6 +2,13 @@ from pydantic import BaseModel
 from typing import Optional
 from pydantic import datetime_parse
 from pydantic_schemas.patient_schema import Patient
+from pydantic_schemas.complaint_schema import Complaint
+from pydantic_schemas.examination_schema import Examination
+from pydantic_schemas.history_schema import History
+from pydantic_schemas.blood_schema import Blood
+from pydantic_schemas.kidney_schema import Kidney
+from pydantic_schemas.liver_schema import Liver
+from pydantic_schemas.assessment_schema import Assessment
 import datetime
 
 # for basic info table
@@ -13,7 +20,14 @@ class Basic(BaseModel):
     sex : str
     ipop_number : int
 
-    patient_basic : Patient
+    patient_patient : Patient
+    patient_complaint : Complaint
+    patient_examination : Examination
+    patient_history : History
+    patient_blood : Blood
+    patient_kidney : Kidney
+    patient_liver : Liver
+    patient_assessment : Assessment
 
     class Config:
         orm_mode = True
